db51a1a829cbf51984764f4a0d2c218936c04622
Bey Alvin Abaasa <bey.abaasa@gmail.com>
2022-06-21 18:25:38 +0300

    home page edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,20 @@ const Hero = () => {
       </p>
       <div className="w-full align-bottom mt-[125px]">
         <div className="bg-secondary w-[800px] h-[138px] rounded-t-lg mx-auto align-bottom items-end justify-end ">
-          <p className="text-black">hi</p>
+          <div class="flex items-center py-2">
+            <input
+              class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder="Jane Doe"
+              aria-label="Full name"
+            />
+            <button
+              class="flex-shrink-0 !bg-primary text-sm text-white py-3 px-5 rounded-lg"
+              type="button"
+            >
+              Search{" "}
+            </button>
+          </div>
         </div>
       </div>
     </div>
