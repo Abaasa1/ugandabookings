4fbd4a7ef6a61215d91329feda2eb7ba7ffc9792
Bey Alvin Abaasa <bey.abaasa@gmail.com>
2022-06-22 18:29:10 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,23 +10,8 @@ const Hero = () => {
         Experience the beauty and the breathtaking scenes unique <br></br>to the
         pearl of Africa.
       </p>
-      <div className="w-full align-bottom mt-[125px]">
-        <div className="bg-secondary w-[800px] h-[138px] rounded-t-lg mx-auto align-bottom items-end justify-end ">
-          <div class="flex items-center py-2">
-            <input
-              class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-              type="text"
-              placeholder="Jane Doe"
-              aria-label="Full name"
-            />
-            <button
-              class="flex-shrink-0 !bg-primary text-sm text-white py-3 px-5 rounded-lg"
-              type="button"
-            >
-              Search{" "}
-            </button>
-          </div>
-        </div>
+      <div className="w-full align-bottom mt-[75px]">
+        <div className="bg-secondary w-[800px] h-[138px] rounded-t-lg mx-auto align-bottom items-end justify-end "></div>
       </div>
     </div>
   );
